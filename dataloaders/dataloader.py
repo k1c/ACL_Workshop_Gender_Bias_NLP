0b877851e1457c27f5ab138d6dbd5cca18ed690a
k1c <cpelletier@klipfolio.com>
2019-04-01 18:33:47 -0400

    general dataloader update

--- dataloaders/dataloader.py
+++ dataloaders/dataloader.py
@@ -10,6 +10,7 @@ There's also a function called preprocess() that cleans sentences (depending on 
 
 from allennlp.predictors import Predictor
 from allennlp.models.archival import load_archive
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 import pandas as pd
 import re
 import unidecode
@@ -26,11 +27,15 @@ import nltk
 #nltk.download('tagsets')
 
 
-class WebDataset(object):
+class Dataloader(object):
     def __init__(self, data_path, output_name, encoding="utf-8"):
         self.data_path = data_path
         self.output_name = output_name
         self.encoding = encoding
+        self.predictor = Predictor.from_archive(
+            load_archive('https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz',
+                         weights_file=None,
+                         overrides=""), 'coreference-resolution')
 
     def preprocess(self, sentences):
         new_data = []
@@ -38,51 +43,52 @@ class WebDataset(object):
             new_sentence = re.sub('"', '', sentence)  # remove quotation marks
             new_sentence = unidecode.unidecode(new_sentence)  # removes accents and represents any unicode to closest ascii
             new_sentence = re.sub('<.*?>', '', new_sentence)  # remove HTML tags (in case, should be clean)
-            new_sentence = re.sub(r'[^\w\s]', '', new_sentence) # remove punctuation
+            # new_sentence = re.sub(r'[^\w\s]', '', new_sentence) # remove punctuation
             # new_sentence = new_sentence.lower() # convert to lower case
             wordcount = len(new_sentence.split())
             if new_sentence != '' and wordcount > 2: # wordcount of 2 or lower doesn't make sense for coref
                 new_data.append(new_sentence)
         return new_data
 
-    def loader(self):
-
-        # Allen NLP coref settings using pre-trained model
-        coref_archive = load_archive('https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz',
-                               weights_file=None,
-                               overrides="")
-
-        predictor = Predictor.from_archive(coref_archive, 'coreference-resolution')
+    def load_IMDB(self):
 
         # read in input_file
         df = pd.read_csv(self.data_path, delimiter='\t', encoding='utf-8', header=None, squeeze=True)
+
+        #info about the dataset
         print("Size of Dataset: ", len(df))
+        print("Columns :", df.columns)
 
         # cleaning sentences
-        df_clean = self.preprocess(df)
+        df_clean = self.preprocess(df[0])
         print("Size of Dataset after clean: ", len(df_clean))
 
-        # get the coref results from allen-nlp
-        coref_results = []
-        for line in df_clean:
+        return df_clean
+
+#Purpose: writes the sentences that have coref-resolution in them to file (one sentence per line)
+    def coref_true_to_file(self, data):
+        # write the coref results to file
+        f = open(self.output_name + "_coref_true.tsv", "w+")
+        for line in data:
             coref_line = {"document":line.strip()}
             try:
-                coref_results.append(predictor.predict_json(coref_line))
+                json = self.predictor.predict_json(coref_line)
             except:
                 print("problem sentence: ", line)
+            if len(json['clusters']) > 0:
+                f.write(TreebankWordDetokenizer().detokenize(json['document'])+"\n")
 
-        # write the coref results to file
-        f = open(self.output_name + ".json", "w+")
-        for i in range(len(coref_results)):
-           string_output = predictor.dump_line(coref_results[i])
-           f.write(string_output)
-        f.close()
+        print("write to file complete")
 
-        print("Size of Dataset after coref: ", len(coref_results))
+# Purpose: writes the sentences that have passed the A1 filter to file (one sentence per line)
+    def A1_filter_to_file(self, data):
+        pass
 
 #Used temporarily for testing
 if __name__ == '__main__':
-    input_path = '../datasets/test_datasets/biasly_data_clean3.tsv'
-    output_name = "biasly_data_testing123"
-    web_data = WebDataset(input_path, output_name)
-    web_data.loader()
+    input_path = '../datasets/test_datasets/IMDB-train.txt'
+    output_name = "IMDB"
+
+    dataloader = Dataloader(input_path, output_name)
+    data = dataloader.load_IMDB()
+    dataloader.coref_true_to_file(data)
