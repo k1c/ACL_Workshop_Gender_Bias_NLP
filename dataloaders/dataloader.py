5be03fce9f303a9512ca4f6a7ae2a31ce8386b9f
k1c <cpelletier@klipfolio.com>
2019-04-25 11:58:42 -0400

    dataloader edits

--- dataloaders/dataloader.py
+++ dataloaders/dataloader.py
@@ -13,7 +13,8 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 pp = pprint.PrettyPrinter(indent=1)
 
 #from loaders import load_IMDB, load_gluternberg
-from filter import remove_sentence
+from filter import check_remove
+from loaders import load_IMDB, load_gutenberg, load_general
 
 #Use the NLTK Downloader to obtain the resources that you need for this script:
 # nltk.download('averaged_perceptron_tagger')
@@ -25,8 +26,7 @@ from filter import remove_sentence
 
 
 class Dataloader(object):
-    def __init__(self, data_path, output_name, filter, encoding="utf-8"):
-        self.data_path = data_path
+    def __init__(self, output_name, filter, encoding="utf-8"):
         self.output_name = output_name
         self.encoding = encoding
         self.filter = filter
@@ -35,41 +35,8 @@ class Dataloader(object):
                          weights_file=None,
                          overrides=""), 'coreference-resolution')
 
-    def preprocess_IMDB(self, data):
-        new_data = []
-        for line in data:
-            new_line = re.sub('<.*?>', ' ', line)  # remove HTML tags and replace with space
-            sentences = sent_tokenize(new_line)  # sentence tokenization
-            for sentence in sentences:
-                new_sentence = re.sub('"', '', sentence)  # remove quotation marks
-                new_sentence = unidecode.unidecode(new_sentence)  # removes accents and represents any unicode to closest ascii
-                new_sentence = re.sub(r'\s+', ' ', new_sentence)  # Eliminate duplicate whitespaces
-                # new_sentence = re.sub(r'[^\w\s]', '', new_sentence) # remove punctuation
-                # new_sentence = new_sentence.lower() # convert to lower case
-                wordcount = len(new_sentence.split())
-                if new_sentence != '' and wordcount > 2: # wordcount of 2 or lower doesn't make sense for coref
-                    new_data.append(new_sentence)
-        return new_data
-
-    # loads IMDB dataset and returns a dataframe that contains one review per line
-    def load_IMDB(self):
-
-        # read in input_file
-        df = pd.read_csv(self.data_path, delimiter='\t', encoding='utf-8', header=None, squeeze=True)
-
-        #info about the dataset
-        print("Size of Dataset: ", len(df))
-        #print("Columns :", df.columns)
-
-        # cleaning sentences
-        df_clean = self.preprocess_IMDB(df)
-        print("Size of Dataset after clean: ", len(df_clean))
-
-        return df_clean
-
-
 # coref_json = {'top_spans': [[0, 0], [0, 8], [2, 5], [2, 8], [7, 8], [12, 12]], 'predicted_antecedents': [-1, -1, -1, -1, -1, 4], 'document': ['This', 'is', 'an', 'action', '-', 'melodrama', 'like', 'the', 'world', 'has', 'never', 'seen', 'it', 'before', '.'], 'clusters': [[[0, 0], [12, 12]]]}
-    def to_file(self, data):
+    def filter_to_file(self, data):
         coref_true = []
         f = open(self.output_name, "w+")
 
@@ -84,7 +51,7 @@ class Dataloader(object):
                 print("problem sentence: ", line)
 
             # if coref cluster exists, then add the coref json to coref_true list
-            if len(coref_json['clusters']) > 0 and self.filter(TreebankWordDetokenizer().detokenize(coref_json['document']),coref_json['clusters'],"all") == False:
+            if len(coref_json['clusters']) > 0 and self.filter(TreebankWordDetokenizer().detokenize(coref_json['document']),coref_json['clusters'],"all") is False:
                 f.write(TreebankWordDetokenizer().detokenize(coref_json['document']) + "\n")
 
         f.close()
@@ -95,8 +62,8 @@ class Dataloader(object):
 #Used temporarily for testing
 if __name__ == '__main__':
     input_path = '../datasets/test_datasets/test_dataset.txt'
-    output_name = "COREF2"
+    output_name = "test_dataset"
 
-    dataloader = Dataloader(input_path, output_name, remove_sentence)
-    data = dataloader.load_IMDB()
-    dataloader.to_file(data)
+    dataloader = Dataloader(output_name, check_remove)
+    data = load_general(input_path)
+    dataloader.filter_to_file(data)
